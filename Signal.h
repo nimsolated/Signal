4817011bd1d0e9b975196dff703c27737d2b5211
nimsolated <armindoosti03@gmail.com>
2025-06-01 00:04:57 -0700

    Modified Signal to get all the return values from all the connected functions contained in a vector (credit to David for the idea)

--- Signal.h
+++ Signal.h
@@ -1,44 +1,53 @@
 #pragma once
-#include <iostream>
 #include <functional>
+#include <iostream>
 #include <vector>
 
 template <typename RetVal, typename... Args>
 class Signal {
 public:
-	// alias for function pointers (uses std::function from the functional library now)
-	using FuncPtr = std::function<RetVal(Args...)>;
+    using FuncPtr = std::function<RetVal(Args...)>;
 public:
-	Signal() = default;
-	Signal(const std::vector<FuncPtr>& functions) : m_functions(functions) {}
-	~Signal() = default;
+    Signal() = default;
+    Signal(const std::vector<FuncPtr>& functions) : m_slots(functions) {}
+    ~Signal() = default;
 
-	// connecting a free function
-	template <typename F>
-	void connect(F&& func) {
-		m_functions.emplace_back(std::forward<F>(func));
-	}
+    // Lambdas, free functions, functors
+    template <typename F>
+    void connect(F&& func) {
+        m_slots.emplace_back(std::forward<F>(func));
+    }
 
-	// connecting a capture-lambda or class member overload
-	template <typename T>
-	void connect(T* objectPtr, RetVal(T::*memberFunc)(Args...)) {
-		m_functions.emplace_back(
-			[objectPtr, memberFunc](Args... args) {
-				(objectPtr->*memberFunc)(std::forward<Args>(args)...);
-			}
-		);
-	}
+    // Member function
+    template <typename T>
+    void connect(T* objectPtr, RetVal (T::*memberFunc)(Args...)) {
+        m_slots.emplace_back([objectPtr, memberFunc](Args... args) {
+            (objectPtr->*memberFunc)(std::forward<Args>(args)...);
+        });
+    }
 
-	// emit all connected functions
-	template <typename... CallArgs>
-	RetVal emit(CallArgs&&... args) {
-		static_assert(sizeof...(CallArgs) == sizeof...(Args), "Wrong number of arguments to emit()");
+    // invoke connected functions
+    template <typename... CallArgs>
+    auto operator()(CallArgs&&... args) {
+        static_assert(sizeof...(CallArgs) == sizeof...(Args), "Wrong number of arguments to emit()");
 
-		for (auto& func : m_functions) {
-			func(std::forward<Args>(args)...);
-		}
-	}
+        // handle void vs non-void return values
+        if constexpr (std::is_void_v<RetVal>) {
+            for (auto& func : m_slots) {
+                func(std::forward<CallArgs>(args)...);
+            }
+        } else {
+            std::vector<RetVal> results;
+            results.reserve(m_slots.size());
+
+            for (auto& func : m_slots) {
+                results.emplace_back(func(std::forward<CallArgs>(args)...));
+            }
+            return results;
+        }
+    }
+
 private:
-	// stores all the connected functions
-	std::vector<FuncPtr> m_functions;
+    // stores all connected functions
+    std::vector<FuncPtr> m_slots;
 };
